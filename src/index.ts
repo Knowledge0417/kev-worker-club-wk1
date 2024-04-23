5defe1b3fd3b013f91f35f89e830aafc3ec0c544
Kevin White <Knowledge0417@gmail.com>
2024-04-23 00:37:47 -0400

    message from VSCode

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,6 @@
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		return new Response('Hello World! from ' + request.url);
+		# Hello from Visual Studio Code!
 	},
 };
